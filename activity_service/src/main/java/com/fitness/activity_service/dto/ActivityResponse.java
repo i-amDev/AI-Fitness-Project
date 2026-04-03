197c18f35b7a618a48b2066476dbc8a8bdd0e95a
i-amDev <92856531+i-amDev@users.noreply.github.com>
2026-04-03 11:28:37 +0530

    - Created the "ActivityResponse" dto class to show the data to the users.

---
+++ activity_service/src/main/java/com/fitness/activity_service/dto/ActivityResponse.java
@@ -0,0 +1,22 @@
+package com.fitness.activity_service.dto;
+
+import com.fitness.activity_service.model.ActivityType;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Data
+@Builder
+public class ActivityResponse {
+    private String id;
+    private String userId;
+    private ActivityType type;
+    private Integer duration;
+    private Integer caloriesBurned;
+    private LocalDateTime startTime;
+    private Map<String, Object> additionalMetrics;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
