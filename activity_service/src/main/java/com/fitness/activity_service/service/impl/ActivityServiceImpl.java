72162f9efd8731f0ac7539eb889c6d8b16f1fd41
i-amDev <92856531+i-amDev@users.noreply.github.com>
2026-04-03 18:14:11 +0530

    - Used the "kafka template to produce the message over kafka". - Corrected the name for the host of "user-service".

--- activity_service/src/main/java/com/fitness/activity_service/service/impl/ActivityServiceImpl.java
+++ activity_service/src/main/java/com/fitness/activity_service/service/impl/ActivityServiceImpl.java
@@ -6,6 +6,8 @@ import com.fitness.activity_service.model.Activity;
 import com.fitness.activity_service.service.ActivityService;
 import com.fitness.activity_service.repository.ActivityRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,6 +17,11 @@ public class ActivityServiceImpl implements ActivityService {
     private final ActivityRepository activityRepository;
 
     private final UserValidationService userValidationService;
+
+    private final KafkaTemplate<String, Activity> kafkaTemplate;
+
+    @Value("${kafka.topic.name}")
+    private String topicName;
 
     @Override
     public ActivityResponse trackActivity(ActivityRequest activityRequest) {
@@ -33,6 +40,11 @@ public class ActivityServiceImpl implements ActivityService {
                 .build();
 
         Activity savedActivity = activityRepository.save(activity);
+        try {
+            kafkaTemplate.send(topicName, savedActivity.getUserId(), savedActivity);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         ActivityResponse activityResponse = ActivityResponse.builder()
                 .id(savedActivity.getId())
